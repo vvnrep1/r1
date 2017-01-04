86284a3d25761845a3f35091fc6b3f06de18a354
venkatgn <venkatadrin.g@gmail.com>
2017-01-04 23:52:29 +0530

    updated array

--- samp_proj/src/test/java/jp1/array_ex.java
+++ samp_proj/src/test/java/jp1/array_ex.java
@@ -1,12 +1,12 @@
 package jp1;
-
+//2nd user
 public class array_ex 
 {
 
 	public static void main(String[] args)
 	{
-		String str[]={"hello","java","selenium"};
-		for(int i=0;i<=2;i++)
+		String str[]={"hello","java","selenium","qtp","linux"};
+		for(int i=0;i<=3;i++)
 			System.out.println(str[i]);
 
 	}
