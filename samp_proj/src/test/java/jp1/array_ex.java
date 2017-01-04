aa48454845939696c6172b9184c1525b4c71238f
venkatgn1 <venkatadrin.g@gmail.com>
2017-01-04 23:58:13 +0530

    final update

--- samp_proj/src/test/java/jp1/array_ex.java
+++ samp_proj/src/test/java/jp1/array_ex.java
@@ -9,16 +9,11 @@ public class array_ex
 
 	public static void main(String[] args)
 	{
-<<<<<<< HEAD
 		String str[]={"hello","java","selenium","qtp","linux"};
 		
-		for(int i=0;i<=3;i++)
-			System.out.println(str[i]);
-=======
-		String str[]={"hello","java","selenium"};
 		for(String x : str)
 			System.out.println(x);
->>>>>>> branch 'master' of https://github.com/vvnrep1/r1.git
+
 
 	}
 
