00a86d3a0757595585bd23b750e4b939598c71f1
venkatgn1 <venkatadrin.g@gmail.com>
2017-01-04 23:54:23 +0530

    dsfsd

--- samp_proj/src/test/java/jp1/array_ex.java
+++ samp_proj/src/test/java/jp1/array_ex.java
@@ -6,6 +6,7 @@ public class array_ex
 	public static void main(String[] args)
 	{
 		String str[]={"hello","java","selenium","qtp","linux"};
+		
 		for(int i=0;i<=3;i++)
 			System.out.println(str[i]);
 
